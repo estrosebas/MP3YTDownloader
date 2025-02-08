19fd62421cf49e074a2fde84c8b126545f1ac2f4
estrosebas <estrosebas@gmail.com>
2025-02-08 22:39:51 +0100

    feat: Refactor download functionality to use a separate downloader module and support cookie file handling

--- src/downloader.py
+++ src/downloader.py
@@ -1,6 +1,6 @@
 import yt_dlp
 
-def download_audio(url, output_dir):
+def download_audio(url, output_dir, cookie_file=None):
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': f'{output_dir}/%(title)s.%(ext)s',
@@ -11,6 +11,11 @@ def download_audio(url, output_dir):
         }],
     }
 
+    # Agregar cookies si se proporcionaron
+    if cookie_file:
+        ydl_opts['cookiefile'] = cookie_file
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+    
     return 'Download complete'
